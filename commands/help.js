9ee4dcc549c25f9709cb8a252f1859cbedfb1cc5
MasterFIN7 <38255441+MasterFIN7@users.noreply.github.com>
2018-07-12 10:38:35 +0300

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  let hEmbed = new Discord.RichEmbed()
+  .setAuthor("KirottuBot", "https://i.imgur.com/mgDKqhT.png")
+  .setThumbnail("https://i.imgur.com/mgDKqhT.png")
+  .setFooter("KirottuBot by: Master#4915", "https://i.imgur.com/mgDKqhT.png")
+  .setColor("#ffffff")
+  .addField("Komennot:", "help\nping\nsay");
+
+  message.channel.send(hEmbed);
+
+}
+
+module.exports.help = {
+  name: "help"
+}
